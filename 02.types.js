ec05938a00a26c3d091778d72d894efbb10cc0d0
Samuel Son <samyeol0930@gmail.com>
2023-09-01 14:53:59 +0900

    add 02.types.js

---
+++ 02.types.js
@@ -0,0 +1,25 @@
+// 숫자
+let age = 25;
+
+// 문자
+let name = 'tiger';
+
+// 불린
+let kind = true;
+
+// 객체
+let user = {
+	age: 25,
+	name: 'tiger',
+	kind: true,
+};
+
+// 배열
+
+// 함수
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof kind);
+console.log(typeof user);
+console.log(typeof 1 + 2);
